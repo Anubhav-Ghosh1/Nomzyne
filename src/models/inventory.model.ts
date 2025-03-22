570f8e1975d74b4e7becc070d5f28f4044c55b71
Anubhav Ghosh <iamanubhav11@gmail.com>
2025-03-22 21:50:24 +0530

    feat: inventory controller

---
+++ src/models/inventory.model.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+interface IInventory extends Document {
+  name: string;
+  barcode: string;
+  password: string;
+  quantity: number;
+  store: mongoose.Types.ObjectId;
+}
+
+const inventorySchema = new mongoose.Schema<IInventory>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true, // make search fast
+    },
+    barcode: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    store: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Store",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<IInventory>("Inventory", inventorySchema);
