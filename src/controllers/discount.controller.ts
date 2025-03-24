6e15360df83ffdc26994f9e620697a5a0c28049b
Anubhav Ghosh <iamanubhav11@gmail.com>
2025-03-24 21:28:55 +0530

    feat: Discount Controller

--- src/controllers/discount.controller.ts
+++ src/controllers/discount.controller.ts
@@ -57,4 +57,88 @@ const createDiscount = asyncHandler(
   }
 );
 
-export { createDiscount };
+const updateDiscount = asyncHandler(
+  async (req: DiscountRequest, res: Response) => {
+    try {
+      const { code, discount, discountCode, storeId } = req.body;
+      const id = req.user._id;
+      const storeExists = await Store.findById(storeId);
+      if (!storeExists) {
+        throw new ApiError(404, "Store not found");
+      }
+      const userExists = await User.findById(id);
+      if (!userExists) {
+        throw new ApiError(404, "User not found");
+      }
+      const discountDetails = await Discount.findById(discountCode);
+      if (!discountDetails) {
+        throw new ApiError(400, "Error while fetching discount details");
+      }
+
+      if (discountDetails.store !== storeId) {
+        throw new ApiError(403, "You are not allowed to update this discount");
+      }
+
+      const updatedDiscount = await Discount.findByIdAndUpdate(
+        discountCode,
+        { code: code, discount: discount },
+        { new: true }
+      );
+
+      return res
+        .status(201)
+        .json(
+          new ApiResponse(201, updatedDiscount, "Discount updated successfully")
+        );
+    } catch (e) {
+      if (e instanceof ApiError) {
+        return res
+          .status(e.statusCode)
+          .json(new ApiResponse(e.statusCode, e.message));
+      }
+    }
+  }
+);
+
+const deleteDiscount = asyncHandler(
+  async (req: DiscountRequest, res: Response) => {
+    try {
+      const { discountCode, storeId } = req.body;
+      const id = req.user._id;
+      const storeExists = await Store.findById(storeId);
+      if (!storeExists) {
+        throw new ApiError(404, "Store not found");
+      }
+      const userExists = await User.findById(id);
+      if (!userExists) {
+        throw new ApiError(404, "User not found");
+      }
+      const discountDetails = await Discount.findById(discountCode);
+      if (!discountDetails) {
+        throw new ApiError(400, "No discount associated with provided id");
+      }
+
+      if (discountDetails.store !== storeId) {
+        throw new ApiError(403, "You are not allowed to update this discount");
+      }
+
+      const updatedDiscount = await Discount.findByIdAndDelete(discountCode, {
+        new: true,
+      });
+
+      return res
+        .status(201)
+        .json(
+          new ApiResponse(201, updatedDiscount, "Discount deleted successfully")
+        );
+    } catch (e) {
+      if (e instanceof ApiError) {
+        return res
+          .status(e.statusCode)
+          .json(new ApiResponse(e.statusCode, e.message));
+      }
+    }
+  }
+);
+
+export { createDiscount, updateDiscount, deleteDiscount };
